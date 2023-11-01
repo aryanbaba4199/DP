8800b9b002268a4492db139f4d311bd4e47c99e9
Aryan Dubey <aryanbaba@gmail.com>
2023-11-01 19:01:25 +0530

    adsense error fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,13 @@
 import '../styles/global.css';
 import { SessionProvider } from 'next-auth/react';
-import { Head } from 'next/document';
+import Head from 'next/head'; // Import the Head component from 'next/head'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <Head>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9500273332762663"
-          crossorigin="anonymous"
-        />
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9500273332762663"
+     crossorigin="anonymous"></script>
       </Head>
 
       <Component {...pageProps} />
